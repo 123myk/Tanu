08f724f242c82f26ea95d9f86abcb798febf119b
123myk <134354964+123myk@users.noreply.github.com>
2023-06-29 11:55:56 +0530

    Add files via upload

---
+++ Controllers/Employee1Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormProgramming.Controllers
+{
+    public class Employee1Controller : Controller
+    {
+        // GET: Employee1
+        [HttpGet]
+        public ActionResult CreateEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateEmployee(FormCollection forms)
+        {
+            string id = forms["EmpCode"];
+            string name = forms["EmpName"];
+            string age = forms["EmpAge"];
+            Response.Write("EmpCode: " + id + "   EmpName: " + name + "   EmpAge: " + age);
+            return View();
+        }
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System.Web.Mvc;
+
+namespace FormProgramming.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [HttpGet]
+        public ActionResult CreateEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateEmployee(string EmpCode,string EmpName,string EmpAge)
+        {
+            Response.Write("EmpCode: "+EmpCode+"   EmpName: "+EmpName+"   EmpAge: "+EmpAge);
+            return View();
+        }
+    }
+}
